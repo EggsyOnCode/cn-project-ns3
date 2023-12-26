434d965793f556eb1991734fd6353ba51f5c7c77
EggsyOnCode <farzansaqib2003@gmail.com>
2023-12-26 08:36:53 +0500

    packet successufly being sent

--- test.cc
+++ test.cc
@@ -2,7 +2,10 @@
 #include "ns3/network-module.h"
 #include "ns3/point-to-point-module.h"
 #include "ns3/internet-module.h"
+#include "ns3/packet-sink.h"
+#include "ns3/applications-module.h"
 #include "ns3/netanim-module.h" // Include NetAnim
+#include "ns3/mobility-module.h" // Include Mobility
 
 using namespace ns3;
 
@@ -43,6 +46,42 @@ int main() {
     anim.SetConstantPosition(nodes.Get(0), 0.0, 0.0); // Set position for node 0
     anim.SetConstantPosition(nodes.Get(49), 100.0, 100.0); // Set position for node 49
 
+    // Add Mobility Models (ConstantPositionMobilityModel)
+    MobilityHelper mobility;
+    Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator>();
+
+    for (uint32_t i = 0; i < nodes.GetN(); ++i) {
+        positionAlloc->Add(Vector(i * 5, i * 5, 0)); // Adjust positions for nodes
+    }
+
+    mobility.SetPositionAllocator(positionAlloc);
+    mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+    mobility.Install(nodes);
+
+    // Simulate TCP communication between nodes 0 and 20
+    Ptr<Node> sender = nodes.Get(0);
+    Ptr<Node> receiver = nodes.Get(20);
+
+    // Install TCP/IP stack on the nodes
+    Ipv4InterfaceContainer interfaces;
+    interfaces = address.Assign(devices);
+
+    // Install TCP applications
+    uint16_t port = 9; // Port number
+    OnOffHelper onoff("ns3::TcpSocketFactory", InetSocketAddress(interfaces.GetAddress(20), port));
+    onoff.SetAttribute("OnTime", StringValue("ns3::ConstantRandomVariable[Constant=1]"));
+    onoff.SetAttribute("OffTime", StringValue("ns3::ConstantRandomVariable[Constant=0]"));
+
+    ApplicationContainer apps = onoff.Install(sender);
+    apps.Start(Seconds(1.0));
+    apps.Stop(Seconds(10.0));
+
+    PacketSinkHelper sinkHelper("ns3::TcpSocketFactory", InetSocketAddress(interfaces.GetAddress(20), port));
+    apps = sinkHelper.Install(receiver);
+    apps.Start(Seconds(0.0));
+    apps.Stop(Seconds(10.0));
+
+    Simulator::Stop(Seconds(10)); // Stop the simulation after 10 seconds
     Simulator::Run();
     Simulator::Destroy();
 
